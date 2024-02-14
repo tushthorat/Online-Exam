3ea918374a645ac1054e265b2432aecac41e6cbc
tushthorat <thoratt49@gmail.com>
2024-02-14 16:51:08 +0530

    first commit

---
+++ src/main/java/com/exam/OnlineExamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExamProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineExamProjectApplication.class, args);
+	}
+
+}
