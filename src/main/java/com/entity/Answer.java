e2804239064a658771b84d5441f62cf4e697aed3
tushthorat <thoratt49@gmail.com>
2024-02-25 22:47:03 +0530

    Created Answer entity

---
+++ src/main/java/com/entity/Answer.java
@@ -0,0 +1,50 @@
+package com.entity;
+
+public class Answer 
+{
+	private int qno;
+	private String qtext;
+	private String submittedAnswer;
+	private String originalAnswer;
+	
+	//{"qno":1,"questiontext":"what is java","submittedAnswer":"A","originalAnswer":"B"}
+	
+	public int getQno() {
+		return qno;
+	}
+	
+	public void setQno(int qno) {
+		this.qno = qno;
+	}
+	
+	public String getQtext() {
+		return qtext;
+	}
+	
+	public void setQtext(String qtext) {
+		this.qtext = qtext;
+	}
+	
+	public String getSubmittedAnswer() {
+		return submittedAnswer;
+	}
+	
+	public void setSubmittedAnswer(String submittedAnswer) {
+		this.submittedAnswer = submittedAnswer;
+	}
+	public String getOriginalAnswer() {
+		return originalAnswer;
+	}
+	public void setOriginalAnswer(String originalAnswer) {
+		this.originalAnswer = originalAnswer;
+	}
+	
+	@Override
+	public String toString() {
+		return "Answer [qno=" + qno + ", qtext=" + qtext + ", submittedAnswer=" + submittedAnswer + ", originalAnswer="
+				+ originalAnswer + "]";
+	}
+	
+	
+	
+}
